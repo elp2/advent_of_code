33e148aed0bf7a2120602f904b9726d38e5c9387
Edward Palmer <edward.palmer@gmail.com>
2019-12-28 11:44:03 +1100

    Day 23 Part 2.

--- day23/day23.py
+++ day23/day23.py
@@ -1,16 +1,42 @@
 from network_computer import NetworkComputer
 
-def part1():
-    computers = list(map(lambda address: NetworkComputer(address), range(0, 50)))
-    while True:
-        for computer in computers:
-            packet = computer.step()
-            if packet:
-                if packet[0] == 255:
-                    print('DONE!: ', packet)
-                    return
-                address = packet[0]
-                print('Packet: ', packet)
-                computers[address].send_packet(packet[1:])
+import sys
 
-# part1() 20665
+def part2():
+    computers = list(map(lambda address: NetworkComputer(address), range(0, 50)))
+    nat_packet = None
+    last_nat_y = None
+
+    sent_packets = 0
+    while True:
+        hungry_computers = []
+        for i, computer in enumerate(computers):
+            packet = computer.step()
+            if computer.is_hungry():
+                hungry_computers.append(i)
+            if packet:
+                sent_packets += 1
+                address = packet[0]
+                packet_contents = packet[1:]
+
+                print('Packet #%d: %s' % (sent_packets, packet))
+                if address == 255:
+                    nat_packet = packet_contents
+                    assert len(nat_packet) == 2
+
+                else:
+                    computers[address].send_packet(packet_contents)
+        # print(len(hungry_computers))
+        if len(hungry_computers) == 50 and sent_packets > 0:
+            assert nat_packet
+            print('Sending NAT packet')
+            computers[0].send_packet(nat_packet)
+
+            if last_nat_y == nat_packet[1]:
+                print('Sent %d to NAT 2x' % (last_nat_y))
+                sys.exit()
+            last_nat_y = nat_packet[1]
+
+            nat_packet = None
+
+part2() # 13358
--- day23/network_computer.py
+++ day23/network_computer.py
@@ -8,6 +8,8 @@ class NetworkComputer:
     def __init__(self, address):
         self.ic = IntCodeComputer(list(map(lambda m: int(m), open('input').readline().split(','))), [address])
         self.ic.debugging = False
+        self.ate_negative_1 = False
+        self.hungry = False
 
     def send_packet(self, packet):
         """Sends the computer a packet."""
@@ -15,14 +17,18 @@ class NetworkComputer:
         if self.ic.inputs == [-1]:
             self.ic.inputs = []
         self.ic.inputs += packet
+        self.hungry = False
 
     def step(self):
         """Steps the computer. Returns an [address, x, y] packet or None if none."""
         assert self.ic.halted == False
         if len(self.ic.inputs) == 0:
-            # No packets.
+            # Either finished or ate a -1 last time.
             self.ic.inputs = [-1]
+        input_is_negative_1 = self.ic.inputs == [-1]
         self.ic.step()
+        if input_is_negative_1:
+            self.hungry = self.hungry or len(self.ic.inputs) == 0
         if len(self.ic.outputs) == 3:
             address_packet = self.ic.outputs
             self.ic.outputs = []
@@ -31,3 +37,6 @@ class NetworkComputer:
 
     def is_halted(self):
         return self.ic.halted
+
+    def is_hungry(self):
+        return self.hungry
