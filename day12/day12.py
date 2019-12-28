caf410250461c2411c5f40ae42f862b99a97f595
Edward Palmer <edward.palmer@gmail.com>
2019-12-28 02:46:08 +0000

    Day 12 Part 2.

--- day12/day12.py
+++ day12/day12.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from itertools import combinations
+
 class MoonSystem:
     def __init__(self):
         self.moons = []
@@ -48,9 +50,47 @@ class MoonSystem:
             energy += potential * kinetic
         return energy
 
-ms = MoonSystem()
-for i in range(0, 1000):
-    ms.step()
+def part1():
+    ms = MoonSystem()
+    for i in range(0, 1000):
+        ms.step()
 
-print(ms.system_energy()) # 7828.
-            
+    print(ms.system_energy()) # 7828.
+
+
+def part2():
+    round = 0
+    ms = MoonSystem()
+
+    # Previous states and when we saw them.
+    previous_states = [defaultdict(lambda: []), defaultdict(lambda: []), defaultdict(lambda: [])]
+    done = [False, False, False]
+    while round < 1000000:
+        if round % 10000 == 0:
+            print(round)
+        for axis in range(0, 3):
+            if done[axis]:
+                continue
+            state = (ms.moons[0]['pos'][axis], ms.moons[1]['pos'][axis], ms.moons[2]['pos'][axis], ms.moons[3]['pos'][axis], ms.moons[0]['vel'][axis], ms.moons[1]['vel'][axis], ms.moons[2]['vel'][axis], ms.moons[3]['vel'][axis])
+            previous_states[axis][state].append(round)
+            if len(previous_states[axis][state]) == 2:
+                done[axis] = True
+        if done == [True, True, True]:
+            break
+        ms.step()
+        round += 1
+
+    for axis in range(0, 3):
+        print(axis)
+        for (state, rounds) in previous_states[axis].items():
+            if len(rounds) > 1:
+                print('[%d] %s -> %s' % (axis, state, rounds))
+
+part2() # 518311327635164 (LCM of the following)
+# Periods for each one as follows:
+# 0
+# [0] (5, 0, 16, 18, 0, 0, 0, 0) -> [0, 186028]
+# 1
+# [1] (-1, -14, 4, 1, 0, 0, 0, 0) -> [0, 231614]
+# 2
+# [2] (5, 2, 0, 16, 0, 0, 0, 0) -> [0, 96236]
