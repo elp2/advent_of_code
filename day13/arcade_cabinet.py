5df672c0c3bad4c1c994b51b2244b8b641fa709f
Edward Palmer <edward.palmer@gmail.com>
2019-12-30 22:36:01 +1100

    Finish Day 13 Part 2.

--- day13/arcade_cabinet.py
+++ day13/arcade_cabinet.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import sys
 sys.path.append('..')
 from intcode.intcode import IntCodeComputer
@@ -12,6 +14,7 @@ BLOCK = 2
 PADDLE = 3
 # 4 is a ball tile. The ball moves diagonally and bounces off objects.
 BALL = 4
+BALL_MOTION = 5
 
 def tile_to_string(tile):
     if tile == EMPTY:
@@ -19,11 +22,13 @@ def tile_to_string(tile):
     elif tile == WALL:
         return '*'
     elif tile == BLOCK:
-        return '_'
+        return '*'
     elif tile == PADDLE:
         return 'V'
     elif tile == BALL:
         return 'B'
+    elif tile == BALL_MOTION:
+        return 'b'
     else:
         assert False
 
@@ -33,6 +38,10 @@ class ArcadeCabinet:
         self.clear_screen()
         self.ball_velocity = [1, 1]
         self.ball_pos = None # Update in first time we see it.
+        self.expected_ball_pos = None
+        self.last_score = None
+        self.board_history = [1,2,3]
+        self.steps = 0
 
     def clear_screen(self):
         self.screen = []
@@ -56,8 +65,12 @@ class ArcadeCabinet:
                 [x, y, tile] = self.ic.outputs
                 self.ic.outputs = []
                 self.add_tile(x, y, tile)
-
-        self.print_screen()
+        if self.last_score != self.score:
+            print('SCORE: %d' % (self.score))
+            self.last_score = self.score
+        self.board_history[self.steps % 3] = self.stringify(self.screen)
+        self.steps += 1
+        self.read_state()
         self.ai_play()
         self.ic.step()
         assert len(self.ic.inputs) == 0
@@ -69,17 +82,12 @@ class ArcadeCabinet:
         else:
             self.screen[y][x] = tile
 
-    def print_screen(self):
-        print('SCORE: %d' % (self.score))
-
+    def read_state(self):
         num_blocks = 0
         for y in range(0, len(self.screen)):
-            line = ''
             for x in range(0, len(self.screen[y])):
                 tile = self.screen[y][x]
-                if tile == BLOCK:
-                    num_blocks += 1
-                elif tile == BALL:
+                if tile == BALL:
                     if self.ball_pos == None:
                         self.ball_pos = (x, y)
                         self.ball_velocity = (1, 1)
@@ -87,77 +95,119 @@ class ArcadeCabinet:
                         old = self.ball_pos
                         self.ball_velocity = (x - old[0], y - old[1])
                         self.ball_pos = (x, y)
-
+                        if self.expected_ball_pos and self.expected_ball_pos != self.ball_pos:
+                            (ex, ey) = self.expected_ball_pos
+                            screen = deepcopy(self.screen)
+                            screen[ey][ex] = BALL_MOTION
+                            print(self.stringify(screen))
+                            print('Velocity:', self.ball_velocity)
+                            i = self.steps % 3
+                            print(self.board_history[i - 2])
+                            print(self.board_history[i - 3])
+                            # assert False, 'Unexpected ball position!'
                 elif tile == PADDLE:
                     self.paddle_pos = (x, y)
-                line += tile_to_string(tile)
-            print(line)
-        
+                elif tile == BLOCK:
+                    num_blocks += 1
+
         if num_blocks == 0:
             print('Destroyed all blocks! Final score: %d' % (self.score))
             sys.exit(0)
 
-    def ball_intersecction_x(self, steps):
+
+    def ball_intersection_x(self, steps):
         """Returns x if ball intersects paddle within [steps] otherwise None."""
-        paddle_y = self.paddle_pos[1] - 1
-        ball_velocity = self.ball_velocity
-        ball_pos = self.ball_pos
-        # assert > ball_pos[1], 'Ball at paddle, YOU LOSE!'
-        screen = self.screen[:]
-        if ball_pos[1] == paddle_y:
-            return ball_pos[0]
+        intersection_y = self.paddle_pos[1] - 1
+
+        screen = deepcopy(self.screen)
         path = []
+
+        [vx, vy] = self.ball_velocity
+        [bx, by] = self.ball_pos
+        assert by <= intersection_y, 'MISSED BALL!'
         while steps != 0:
-            next = (ball_pos[0] + ball_velocity[0], ball_pos[1] + ball_velocity[1])
-            if next[1] == paddle_y:
-                return next[0]
-            elif next[1] == paddle_y + 1:
-                assert False, 'YOU LOSE!'
-            tile = screen[next[1]][next[0]]
+            path.append((bx, by))
+            if by == intersection_y:
+                break
 
+            loop = 5
             while True:
-                [bx, by] = ball_pos
-                [vx, vy] = ball_velocity
-                hit_block = False
-                if screen[by][bx + vx] == BLOCK:
-                    screen[by][bx + vx] = EMPTY
-                    vx *= -1
-                    hit_block = True
-                elif screen[by + vy][bx] == BLOCK:
-                    screen[by + vy][bx] = EMPTY
+                loop -= 1
+                if loop == 0:
+                    print('loop=0?')
+                nx = bx + vx
+                ny = by + vy
+                block_x = screen[by][nx] == BLOCK
+                block_y = screen[ny][bx] == BLOCK
+                if block_x and block_y:
+                    screen[ny][bx] = EMPTY
                     vy *= -1
-                    hit_block = True
-                if not hit_block:
+                    screen[by][nx] = EMPTY
+                    vx *= -1
+                elif block_y:
+                    screen[ny][bx] = EMPTY
+                    vy *= -1
+                elif block_x:
+                    screen[by][nx] = EMPTY
+                    vy *= -1
+                elif screen[ny][bx] == WALL:
+                    vy *= -1
+                elif screen[by][nx] == WALL:
+                    vx *= -1
+                elif screen[ny][nx] == EMPTY or screen[ny][nx] == BALL:
                     break
+                elif screen[ny][nx] == BLOCK:
+                    screen[ny][nx] = EMPTY
+                    vx *= -1
+                    vy *= -1
 
-            tile = screen[next[1]][next[0]]
-            if tile == WALL:
-                ball_velocity = (-1 * ball_velocity[0], ball_velocity[1])
-                next = (ball_pos[0] + ball_velocity[0], ball_pos[1] + ball_velocity[1])
-            path.append(next)
-            ball_pos = next
+            assert abs(bx - nx) == 1
+            assert abs(by - ny) == 1
+            bx = nx
+            by = ny
             steps -= 1
-        print('No intersection following %s' % (path))
-        if self.ball_pos[0] > self.paddle_pos[0]:
-            return 1
-        elif elf.ball_pos[0] > self.paddle_pos[0]:
-            return -1
-        return None
+
+        if path[-1][1] == intersection_y:
+            if len(path) > 1:
+                self.expected_ball_pos = path[1]
+            else:
+                self.expected_ball_pos = None
+
+            return path[-1][0]
+        else:
+            # print('No intersection following %s' % (path))
+            # for p in path:
+            #     screen[p[1]][p[0]] = BALL_MOTION
+            # print(self.stringify(screen))
+            # assert False
+            return None
+
+    def stringify(self, screen):
+        ret = ''
+        for y in range(0, len(screen)):
+            line = ''
+            for x in range(0, len(screen[y])):
+                line += tile_to_string(screen[y][x])
+            ret += line + '\n'
+        return ret
 
     def ai_play(self):
         """Move the paddle to where the ball will be."""
-        intersection = self.ball_intersecction_x(15)
+        intersection = self.ball_intersection_x(2000)
         if intersection == None:
-            print('HOLD')
             move = 0
         elif intersection == self.paddle_pos[0]:
-            print('_')
             move = 0
         elif intersection > self.paddle_pos[0]:
-            print('>')
             move = 1
         elif intersection < self.paddle_pos[0]:
-            print('<')
             move = -1
         assert len(self.ic.inputs) == 0
         self.ic.inputs = [move]
+
+def part2():
+    ac = ArcadeCabinet()
+    ac.play()
+
+if __name__ == "__main__":
+    part2()
