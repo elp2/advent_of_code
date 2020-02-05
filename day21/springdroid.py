d879365fccb5015f5d88a86fc3ab88cb7bfa070e
Edward Palmer <edward.palmer@gmail.com>
2020-02-04 22:22:44 -0800

    Solve Day 21 Part 2.

--- day21/springdroid.py
+++ day21/springdroid.py
@@ -26,21 +26,53 @@ class SpringDroid:
         print(out)
         if score:
             print('SCORE: %d' % (score))
+            return score
         return False
 
-# .
-# ...
-# .##.
+#     If one of the first three is empty, and we can jump to the 4th, do it.
+BASE = """NOT A J
+    NOT B T
+    OR T J
+    NOT C T
+    OR T J
+    AND D J
+    """
 
 def part1():
     sd = SpringDroid()
-    sd.execute('NOT C J\nAND D J\nNOT A T\nOR T J\nWALK\n')
+    assert sd.execute(BASE + 'WALK\n') == 19350938
     # 19350938
 
 def part2():
     sd = SpringDroid()
-    sd.execute('NOT C J\nAND D J\nNOT A T\nOR T J\nRUN\n')
+
+    # .ABCDEFGHI
+    #      ABCDEFGHI
+    # We need to find a path from A->D->H (maybe I?).
+    # A->D->
+    #       (Jump) H, walk to I or jump past.
+    #      -> E -Jump-> I
+    #           -> F -jump> pos after I
+    # D and ((E AND (F OR I)) OR H)
+
+
+    script = BASE + """NOT F T
+    NOT T T
+    """ # T = F
+    script += """OR I T
+    """ #T = F OR I
+    script += """AND E T
+    """ # T = (F OR I) AND E
+    script += """OR H T
+    """ # T = H OR ((F OR I) AND E)
+    script += """AND T J
+    """ # Jump if we have a path AND above.
+
+
+    script += 'RUN\n'
+    assert sd.execute(script) == 1142986901
 
 
 if __name__ == "__main__":
+    part1()
     part2()
