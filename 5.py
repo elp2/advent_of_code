a0f4ab2bad820b656a0de73bf790d9e46403a747
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-23 07:58:40 -0800

    Day 5 Part 2.

--- 5.py
+++ 5.py
@@ -1,5 +1,5 @@
-def execute(input):
-    instructions = list(map(int, input))
+def execute(jump_strings):
+    instructions = list(map(int, jump_strings))
     pc = 0
     steps = 0
     while pc >= 0 and pc < len(instructions):
@@ -17,3 +17,25 @@ def execute(input):
 
 # print(execute(open("5.txt").readlines())) # 364539
 
+def execute2(jump_strings):
+    instructions = list(map(int, jump_strings))
+    pc = 0
+    steps = 0
+    while pc >= 0 and pc < len(instructions):
+        steps += 1
+        jump = instructions[pc]
+        if jump >= 3:
+            instructions[pc] -= 1
+        else:
+            instructions[pc] += 1
+        pc += jump
+    return steps
+
+
+assert 10 == execute2("""0
+3
+0
+1
+-3""".split("\n"))
+
+print(execute2(open("5.txt").readlines())) # 27477714
