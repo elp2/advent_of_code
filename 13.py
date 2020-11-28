3dc39b3a2d8a8e3f376546fdc3c8ac6084430126
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-27 19:22:22 -0800

    Day 13 Part 2.

--- 13.py
+++ 13.py
@@ -7,28 +7,36 @@ def parse_barriers(lines):
         layer = int(layer)
         while len(barriers) < layer:
             barriers.append(None)
+        middle = max(range - 2, 0)
+        end = 1
+        range = end + middle + end + middle
         barriers.append(range)
     return barriers
 
 sample = parse_barriers(open("13.sample").readlines())
 print(sample)
 
-def score_barriers(barriers):
+def score_barriers(barriers, delay=0):
     score = 0
     for layer in range(len(barriers)):
         b = barriers[layer]
         if not b:
             continue
-        middle = max(b-2, 0)
-        end = 1
-        cycle = end + middle + end + middle
-        bpos = layer % cycle
+        bpos = (layer + delay) % b
         if bpos == 0:
-            print('Hit at ', layer)
+            return 1 # BROKEN for Part 1.
+            # print('Hit at ', layer)
             score += layer * b
     return score
 
 print(score_barriers(sample))
 
 real = parse_barriers(open("13.txt").readlines())
-print(score_barriers(real)) # 1588
+
+delay = 0
+while True:
+    delay += 1
+    score = score_barriers(real, delay=delay)
+    if score == 0:
+        print(delay) # 3865118
+        break
