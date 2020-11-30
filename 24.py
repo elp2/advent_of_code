85f6076988ac1ad9d5002afab057a7e35d09b756
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-29 21:34:43 -0800

    Day 24 Part 2.

--- 24.py
+++ 24.py
@@ -28,7 +28,38 @@ def part1(lines):
         return best
     return best_score(comps, 0, 0)
 
-samp = part1(SAMPLE)
-assert 31 == samp
+# samp = part1(SAMPLE)
+# assert 31 == samp
 
-print(part1(REAL)) # 1656
+# print(part1(REAL)) # 1656
+
+def part2(lines):
+    comps = parse_components(lines)
+    def best_score(components, match, prev_score, here_len):
+        best = prev_score
+        best_len = here_len
+        for i in range(len(components)):
+            c = components[i]
+            a, b = c
+            # assert a != b v
+            if a == match or b == match:
+                if a == match:
+                    to_match = b
+                else:
+                    to_match = a
+                new_comp = components[:i] + components[i + 1:]
+                new_score, new_len = best_score(new_comp, to_match, prev_score + a + b, here_len + 1)
+                if new_len > best_len:
+                    best = new_score
+                    best_len = new_len
+                elif new_len == best_len:
+                    if new_score > best:
+                        best = new_score
+        return (best, best_len)
+    return best_score(comps, 0, 0, 0)
+
+samp = part2(SAMPLE)
+print(samp)
+assert 19 == samp[0]
+
+print(part2(REAL)) # 1656 - too high # 1642
