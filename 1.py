2c487bb36ce36f2b1642df80911af28c1eb3fbae
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-30 21:13:49 -0800

    Day 1.

---
+++ 1.py
@@ -0,0 +1,39 @@
+REAL=open("1.txt").readlines()
+SAMPLE=open("1.sample").readlines()
+
+def parse_lines(lines):
+    return list(map(int, lines))
+
+def part1(lines):
+    parsed = parse_lines(lines)
+    for i in range(len(parsed) - 2):
+        for j in range(i + 1, len(parsed) - 1):
+            for k in range(j + 1, len(parsed)):
+                a = int(parsed[i])
+                b = int(parsed[j])
+                c = int(parsed[k])
+
+                if a + b + c == 2020:
+                    print(a,b,c)
+                    return a * b * c
+    return ret
+
+# assert part1(SAMPLE) == 514579
+# print(part1(REAL))
+
+def part2(lines):
+    parsed = parse_lines(lines)
+    for i in range(len(parsed) - 2):
+        for j in range(i + 1, len(parsed) - 1):
+            for k in range(j + 1, len(parsed)):
+                a = int(parsed[i])
+                b = int(parsed[j])
+                c = int(parsed[k])
+
+                if a + b + c == 2020:
+                    print(a,b,c)
+                    return a * b * c
+    return ret
+
+assert part2(SAMPLE) == 241861950
+print(part2(REAL)) # 236430480
