79396ba47f706fa8446abf04f5348d5211131307
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-30 20:58:27 -0800

    Solution template.

---
+++ template.py
@@ -0,0 +1,21 @@
+REAL=open("1.txt").readlines()
+SAMPLE=open("1.sample").readlines()
+
+def parse_lines(lines):
+
+
+def part1(lines):
+    parsed = parse_lines(lines)
+    ret = 0
+    return ret
+
+assert part1(SAMPLE) == 1
+print(part1(REAL))
+
+def part2(lines):
+    parsed = parse_lines(lines)
+    ret = 0
+    return ret
+
+# assert part2(SAMPLE) == 1
+# print(part2(REAL))
