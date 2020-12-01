2727aa8fe559d09d4a4f3c0932631c907158b9c9
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-30 21:24:30 -0800

    Clean up Day 1.

--- 1.py
+++ 1.py
@@ -8,30 +8,28 @@ def part1(lines):
     parsed = parse_lines(lines)
     for i in range(len(parsed) - 2):
         for j in range(i + 1, len(parsed) - 1):
-            for k in range(j + 1, len(parsed)):
-                a = int(parsed[i])
-                b = int(parsed[j])
-                c = int(parsed[k])
+            a = parsed[i]
+            b = parsed[j]
 
-                if a + b + c == 2020:
-                    print(a,b,c)
-                    return a * b * c
+            if a + b == 2020:
+                print(a, b)
+                return a * b
     return ret
 
-# assert part1(SAMPLE) == 514579
-# print(part1(REAL))
+assert part1(SAMPLE) == 514579
+print(part1(REAL))
 
 def part2(lines):
     parsed = parse_lines(lines)
     for i in range(len(parsed) - 2):
         for j in range(i + 1, len(parsed) - 1):
             for k in range(j + 1, len(parsed)):
-                a = int(parsed[i])
-                b = int(parsed[j])
-                c = int(parsed[k])
+                a = parsed[i]
+                b = parsed[j]
+                c = parsed[k]
 
                 if a + b + c == 2020:
-                    print(a,b,c)
+                    print(a, b, c)
                     return a * b * c
     return ret
 
