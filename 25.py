b64d395a3cf73e09cc8034478daacf069d634718
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-11-30 21:58:05 -0800

    Day 25 Part 1.

--- 25.py
+++ 25.py
@@ -1,67 +1,50 @@
-REAL={
-    "A": {
-        0: [1, 1, "B"],
-        1: [0, -1, "D"],
-    },
-    "B": {
-        0: [1, 1, "C"],
-        1: [0, 1, "F"],
-    },
-    "C": {
-        0: [1, -1, "C"],
-        1: [0, -1, "A"],
-    },
-    "D": {
-        0: [0, -1, "E"],
-        1: [1, 1, "A"],
-    },
-    "E": {
-        0: [1, -1, "A"],
-        1: [0, 1, "B"],
-    },
-    "F": {
-        0: [0, 1, "C"],
-        1: [0, 1, "E"],
-    },
-}
 
 REAL_STEPS=12302209
-REAL_START="A"
+
+a, b, c, d, e, f = range(6)
+right = 1
+left = -1
 
 SAMPLE={
-    "A": {
-        0: [1, 1, "B"],
-        1: [0, -1, "B"],
-    },
-    "B": {
-        0: [1, -1, "A"],
-        1: [1, 1, "A"],
-    },
+    (a, 0): [1, right, b],
+    (a, 1): [0, left, b],
+    (b, 0): [1, left, a],
+    (b, 1): [1, right, a],
+}
+
+REAL= {
+    (a, 0): [1, right, b],
+    (a, 1): [0, left, d],
+    (b, 0): [1, right, c],
+    (b, 1): [0, right, f],
+    (c, 0): [1, left, c],
+    (c, 1): [1, left, a],
+    (d, 0): [0, left, e],
+    (d, 1): [1, right, a],
+    (e, 0): [1, left, a],
+    (e, 1): [0, right, b],
+    (f, 0): [0, right, c],
+    (f, 1): [0, right, e],
 }
 
 from collections import defaultdict
 def return_zero():
     return 0
+
+
 def part1(state, transitions, steps):
     tape = defaultdict(return_zero)
     pos = 0
-    ones = 0
     for _ in range(steps):
         here = tape[pos]
-        new_here, direction, new_state = transitions[state][here]
-
-        if here == 1 and new_here == 0:
-            ones -= 1
-        if here == 0 and new_here == 1:
-            ones += 1
+        new_here, direction, new_state = transitions[(state, here)]
 
         tape[pos] = new_here
         pos += direction
         state = new_state
-    print(ones)
-    print(sum(tape.values()))
-    return ones
+    return sum(tape.values())
 
-assert 3 == part1("A", SAMPLE, 6)
-part1(REAL_START, REAL, REAL_STEPS) # 1118384 high
+
+assert 3 == part1(a, SAMPLE, 6)
+print(part1(a, REAL, REAL_STEPS)) # 1118384 high # 4100737 no # 633 YES
  
