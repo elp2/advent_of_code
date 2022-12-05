7a9f1482f59d976fc0a3ecef172ed865791a11dd
Edward Palmer <891364+elp2@users.noreply.github.com>
2022-12-04 20:23:56 -0800

    2022 Day 4.

---
+++ 2022/4.py
@@ -0,0 +1,71 @@
+from collections import defaultdict, deque, Counter
+from itertools import combinations, combinations_with_replacement, permutations
+from functools import reduce
+import math
+# import numpy as np
+from operator import add, mul, itemgetter, attrgetter
+import re
+
+DS = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+DS8 = DS + [[-1, -1], [1, -1], [-1, 1], [1, 1]]
+def arounds_inside(x, y, diagonals, w, h):
+    ret = []
+    for dx, dy in DS8 if diagonals else DS:
+        ret.append((x + dx, y + dy))
+
+
+CHALLENGE_DAY = "4"
+REAL = open(CHALLENGE_DAY + ".txt").read()
+
+SAMPLE_EXPECTED = 2
+if SAMPLE_EXPECTED:
+    SAMPLE = open(CHALLENGE_DAY + ".s.txt").read()
+
+
+def parse_lines(raw):
+    # Groups.
+    # groups = raw.split("\n\n")
+    # return list(map(lambda group: group.split("\n"), groups))
+    lines = raw.split("\n")
+    ret = []
+    for l in lines:
+        elves = l.split(",")
+        row = []
+        for e in elves:
+            s, e = e.split("-")
+            row.append((int(s), int(e)))
+        ret.append(row)
+
+    return ret
+
+    # return lines # raw
+    # return list(map(lambda l: l.split(" "), lines)) # words.
+    # return list(map(int, lines))
+    # return list(map(lambda l: l.strip(), lines)) # beware leading / trailing WS
+
+def solve(raw):
+    parsed = parse_lines(raw)
+    # Debug here to make sure parsing is good.
+    ret = 0
+
+    for ((es1, ee1), (es2, ee2)) in parsed:
+        if es1 >= es2 and ee1 <= ee2 or es2 >= es1 and ee2 <= ee1:
+            ret += 1
+
+    return ret
+
+if SAMPLE_EXPECTED != None:
+    sample = solve(SAMPLE)
+    if sample != SAMPLE_EXPECTED:
+        print("SAMPLE FAILED: ", sample, " != ", SAMPLE_EXPECTED)
+    assert sample == SAMPLE_EXPECTED
+    print("\n*** SAMPLE PASSED ***\n")
+else:
+    print("Skipping sample")
+
+solved = solve(REAL)
+print("SOLUTION: ", solved)
+import pandas as pd
+df=pd.DataFrame([str(solved)])
+df.to_clipboard(index=False,header=False)
+print("COPIED TO CLIPBOARD")
---
+++ 2022/4p2.py
@@ -0,0 +1,68 @@
+from collections import defaultdict, deque, Counter
+from itertools import combinations, combinations_with_replacement, permutations
+from functools import reduce
+import math
+# import numpy as np
+from operator import add, mul, itemgetter, attrgetter
+import re
+
+DS = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+DS8 = DS + [[-1, -1], [1, -1], [-1, 1], [1, 1]]
+def arounds_inside(x, y, diagonals, w, h):
+    ret = []
+    for dx, dy in DS8 if diagonals else DS:
+        ret.append((x + dx, y + dy))
+
+
+CHALLENGE_DAY = "4"
+REAL = open(CHALLENGE_DAY + ".txt").read()
+
+SAMPLE_EXPECTED = 4
+if SAMPLE_EXPECTED:
+    SAMPLE = open(CHALLENGE_DAY + ".s.txt").read()
+
+
+def parse_lines(raw):
+    # Groups.
+    # groups = raw.split("\n\n")
+    # return list(map(lambda group: group.split("\n"), groups))
+    lines = raw.split("\n")
+    ret = []
+    for l in lines:
+        elves = l.split(",")
+        row = []
+        for e in elves:
+            s, e = e.split("-")
+            row.append((int(s), int(e)))
+        ret.append(row)
+
+    return ret
+
+    # return lines # raw
+    # return list(map(lambda l: l.split(" "), lines)) # words.
+    # return list(map(int, lines))
+    # return list(map(lambda l: l.strip(), lines)) # beware leading / trailing WS
+
+def solve(raw):
+    parsed = parse_lines(raw)
+    # Debug here to make sure parsing is good.
+    ret = 0
+
+    for ((es1, ee1), (es2, ee2)) in parsed:
+        if es1 <= es2 <= ee1 or es2 <= es1 <= ee2:
+            ret += 1
+
+    return ret
+
+if SAMPLE_EXPECTED != None:
+    sample = solve(SAMPLE)
+    if sample != SAMPLE_EXPECTED:
+        print("SAMPLE FAILED: ", sample, " != ", SAMPLE_EXPECTED)
+    assert sample == SAMPLE_EXPECTED
+    print("\n*** SAMPLE PASSED ***\n")
+else:
+    print("Skipping sample")
+
+solved = solve(REAL)
+print("SOLUTION: ", solved)
+
