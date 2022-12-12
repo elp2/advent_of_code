cf31959240e49a00768e4c1bd5089e4176b9b4fe
Edward Palmer <891364+elp2@users.noreply.github.com>
2022-12-11 20:00:37 -0800

    2022 Day 11 ala dataclass.

--- 2022/11p2.py
+++ 2022/11p2.py
@@ -1,10 +1,12 @@
 from collections import defaultdict, deque, Counter
+from dataclasses import dataclass
 from itertools import combinations, combinations_with_replacement, permutations
 from functools import reduce
 import math
 # import numpy as np
 from operator import add, mul, itemgetter, attrgetter
 import re
+from typing import List
 
 DS = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 DS8 = DS + [[-1, -1], [1, -1], [-1, 1], [1, 1]]
@@ -22,42 +24,51 @@ if SAMPLE_EXPECTED:
     SAMPLE = open(CHALLENGE_DAY + ".s.txt").read()
 
 
-# addx V takes two cycles to complete. After two cycles, the X register is increased by the value V. (V can be negative.)
-# noop takes one cycle to complete. It has no other effect.
+@dataclass
+class Monkey:
+    name: str
+    items: deque[int]
+    operation: str
+    true_monkey: int
+    false_monkey: int
+    divisible: int
+    num_inspects: int = 0
 
-MNAME="NAME"
-ITEMS="items"
-OPERATION="operation"
-TEST="test"
-MTRUE="mtrue"
-MFALSE="mfalse"
-MINSPECTS="inspects"
-MDIVISIBLE="MDIVISIBLE"
+    def __init__(self, s):
+        s = s.split("\n")
+#Monkey 1:
+        self.name = s[0]
+#   Starting items: 76, 62, 61, 54, 69, 60, 85
+        self.items = deque(map(int, re.findall(r"[0-9]+", s[1])))
+#   Operation: new = old + 1
+        self.operation = s[2].split(" = ")[1]
+#   Test: divisible by 17
+        self.divisible = int(s[3].split(" ")[-1])
+#     If true: throw to monkey 0
+        self.true_monkey = int(s[4].split(" ")[-1])
+#     If false: throw to monkey 6
+        self.false_monkey = int(s[5].split(" ")[-1])
+
+    def turn(self, reducer):
+        if not len(self.items):
+            return None
+        self.num_inspects += 1
+        item = self.items.popleft()
+        old = item % reducer
+        item = eval(self.operation)
+
+        if item % self.divisible == 0:
+            return(self.true_monkey, item)
+        else:
+            return(self.false_monkey, item)
+
 
 def parse_lines(raw):
     # Groups.
     monkeys = []
     groups = raw.split("\n\n")
     for mt in groups:
-        ml = mt.split("\n")
-        # Monkey 2:
-        #   Starting items: 79, 60, 97
-        #   Operation: new = old * old
-        #   Test: divisible by 13
-        #     If true: throw to monkey 1
-        #     If false: throw to monkey 3
-
-        monkey = {}
-        monkey[MNAME] = ml[0]
-        monkey[MINSPECTS] = 0
-        items = ml[1].split(": ")[1].split(", ")
-        items = deque(list(map(int, items)))
-        monkey[ITEMS] = items
-        monkey[OPERATION] = ml[2].split( " = ")[1]
-        monkey[MDIVISIBLE] = int(ml[3].split(" ")[5])
-        monkey[MTRUE] = int(ml[-2].split(" ")[-1])
-        monkey[MFALSE] = int(ml[-1].split(" ")[-1])
-
+        monkey = Monkey(mt)
         print(monkey)
         monkeys.append(monkey)
     return monkeys
@@ -65,35 +76,26 @@ def parse_lines(raw):
 
 def solve(raw):
     monkeys = parse_lines(raw)
-    mods = map(lambda m: m[MDIVISIBLE], monkeys)
+    mods = map(lambda m: m.divisible, monkeys)
     reducer = reduce(mul, mods)
-    for round in range(10000):
-
-        
+    for round in range(10000):        
         if round % 100 == 0:
             print(round)
         for m in monkeys:
             while True:
-                if not len(m[ITEMS]):
-                    break
-                m[MINSPECTS] += 1
-                old = m[ITEMS].popleft()
-                old = old % reducer
-#   Monkey inspects an item with a worry level of 79.
-#     Worry level is multiplied by 19 to 1501.
-                item = eval(m[OPERATION])
-                # item = item // 3
-                if item % m[MDIVISIBLE] == 0:
-                    monkeys[m[MTRUE]][ITEMS].append(item)
+                turn = m.turn(reducer)
+                if turn:
+                    recipient, item = turn
+                    monkeys[recipient].items.append(item)
                 else:
-                    monkeys[m[MFALSE]][ITEMS].append(item)
+                    break
 
 
 #     Monkey gets bored with item. Worry level is divided by 3 to 500.
 #     Current worry level is not divisible by 23.
 #     Item with worry level 500 is thrown to monkey 3.
     print(monkeys)
-    inspects = map(lambda m: m[MINSPECTS], monkeys)
+    inspects = map(lambda m: m.num_inspects, monkeys)
     si = sorted(inspects)
     return si[-1] * si[-2]
 
