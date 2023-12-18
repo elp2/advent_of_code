c735f0204c7c796ab1269487031ade7b5d966949
Edward Palmer <891364+elp2@users.noreply.github.com>
2023-12-18 04:44:07 +0000

    2023 Day 2 Part 2.

--- 2023/src/day02/index.ts
+++ 2023/src/day02/index.ts
@@ -42,7 +42,6 @@ const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   // console.log(input);
   let cubes = {"red": 12, "green": 13, "blue": 14};
-  console.log(input["100"])
   let ret = 0;
   for (let [gameId, game] of Object.entries(input)) {
 
@@ -56,8 +55,20 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  let ret = 0;
 
-  return;
+  for (let [gameId, game] of Object.entries(input)) {
+    let mins = {"red": 0, "green": 0, "blue": 0};
+    for (let draw of game) {
+      for (let [color, colorNum] of Object.entries(draw)) {
+        if (colorNum > mins[color]) {
+          mins[color] = colorNum;
+        }
+      }
+    }
+    ret += mins["red"] * mins["green"] * mins["blue"];
+  }
+  return ret;
 };
 
 run({
@@ -76,10 +87,14 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+        expected: 2286,
+      },
     ],
     solution: part2,
   },
