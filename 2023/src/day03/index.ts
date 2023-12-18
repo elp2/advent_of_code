54f3e97293d29b58acca5d2fa89420afd82d4ad7
Edward Palmer <891364+elp2@users.noreply.github.com>
2023-12-18 15:40:46 +0000

    2023 Day 3 Part 2.

--- 2023/src/day03/index.ts
+++ 2023/src/day03/index.ts
@@ -66,22 +66,87 @@ const parseInput = (rawInput: string) => {
   return ret;
 };
 
+const parseInput2 = (rawInput: string) => {
+  let grid = [];
+  for (let line of rawInput.split("\n")) {
+    line = line.trim();
+    grid.push(line);
+  }
+  let gears = {};
+
+  let ret = {};
+  for (let y = 0; y < grid.length; y++) {
+    let line = grid[y];
+    for (let x = 0; x < line.length; x++) {
+      let c = line[x];
+      if (!NUMBERS.includes(c)) {
+        continue;
+      }
+
+      let name = "";
+      let symbol = EMPTY;
+      let gx = 0;
+      let gy = 0;
+      while (x < line.length) {
+        c = line[x];
+        if (!NUMBERS.includes(c)) {
+          break;
+        }
+        name += c;
+
+        for (let dx = x - 1; dx < x + 2; dx++) {
+          for (let dy = y - 1; dy < y + 2; dy++) {
+            let a = at(grid, dx, dy);
+            if (a == "*") {
+              symbol = a;
+              gx = dx;
+              gy = dy;
+            }
+          }
+        }
+
+        x += 1;
+      }
+      if (symbol != EMPTY) {
+        let gk = "" + gx + "_" + gy;
+        if (!(gk in gears)) {
+          gears[gk] = [];
+        }
+        gears[gk].push(name);
+//        ret[name] = symbol;
+
+      }
+    }
+  }
+
+  // console.log(ret);
+  return gears;
+};
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   let ret = 0;
   for (let [num, parts] of Object.entries(input)) {
     for (let parttype of parts) {
-      ret += parseInt(num); // parttype;
+      ret += parseInt(num);
     }
   }
   return ret;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const gears = parseInput2(rawInput);
+  // console.log(gears);
 
-  return;
+  let ret = 0;
+  for (let [gk, parts] of Object.entries(gears)) {
+    if (parts.length == 2) {
+      ret += parts[0] * parts[1];
+    }
+  }
+
+  return ret;
 };
 
 run({
@@ -105,10 +170,19 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..`,
+        expected: 467835,
+      },
     ],
     solution: part2,
   },
