10c1554d70eba97cedc884e110b657980123dccb
Edward Palmer <891364+elp2@users.noreply.github.com>
2023-12-18 16:16:32 +0000

    2023 Day 4 Part 2.

--- 2023/src/day04/index.ts
+++ 2023/src/day04/index.ts
@@ -34,9 +34,7 @@ const part1 = (rawInput: string) => {
     let have = c[1];
     let intersect = new Set([...winners].filter(i => have.has(i)));
     if (intersect.size > 0) {
-      console.log(c, ret);
       let points = Math.pow(2, intersect.size - 1);
-      console.log(points);
       ret += points;
     }
   }
@@ -45,11 +43,28 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const cards = parseInput(rawInput);
+  const holds = [];
+  for (let c of cards) {
+    holds.push(1);
+  }
 
-  return;
+  let ret = 0;
+  for (let i = 0; i < cards.length; i++) {
+    let c = cards[i];
+    let hereholds = holds[i];
+
+    let winners = c[0];
+    let have = c[1];
+    let intersect = new Set([...winners].filter(i => have.has(i)));
+    let points = intersect.size;
+    for (let pi = 0; pi < points; pi++) {
+      holds[i + 1 + pi] += hereholds;
+    }
+  }
+
+  return holds.reduce((sum, current) => sum + current, 0);
 };
-
 run({
   part1: {
     tests: [
@@ -67,10 +82,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 30,
+      },
     ],
     solution: part2,
   },
