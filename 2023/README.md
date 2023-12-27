28f32c6b4b8b15ca41dfc869fa3d1f9de2074c76
Edward Palmer <891364+elp2@users.noreply.github.com>
2023-12-27 08:03:50 +0000

    2023 Day 13 Part 1.

--- 2023/README.md
+++ 2023/README.md
@@ -23,7 +23,7 @@
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%86/yellow)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
-![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%86/yellow)](src/day13)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
@@ -153,9 +153,9 @@ Both parts: 180.357ms
 
 ```
 Day 13
-Time part 1: -
+Time part 1: 1.443ms
 Time part 2: -
-Both parts: -
+Both parts: 1.443ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 23/50
-Total time: 697.408ms
+Total stars: 24/50
+Total time: 698.851ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day13/index.ts
+++ 2023/src/day13/index.ts
@@ -1,11 +1,55 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  let ret = [];
+
+  let squares = rawInput.split("\n\n");
+  for (let s of squares) {
+    let orig = s.split("\n");
+    let rotate = [];
+    for (let x = 0; x < orig[0].length; x++) {
+      let row = "";
+      for (let y = 0; y < orig.length; y++) {
+        row += orig[y][x];
+      }
+      rotate.push(row);
+    }
+
+    let rotatesAt = (sq: [string]) => {
+      for (let astart = 0; astart < sq.length - 1; astart++) {
+        let a = astart;
+        let b = astart + 1;
+        while (a >= 0 && b < sq.length && (sq[a] == sq[b])) {
+          a -= 1;
+          b += 1;
+        }
+        if (a < 0 || b >= sq.length) {
+          return astart + 1; // 1 indexed
+        }
+      }
+      return null;
+    }
+    ret.push([orig, rotate, rotatesAt(orig), rotatesAt(rotate)]);
+  }
+  return ret;
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  // console.log(input);
 
-  return;
+  let ret = 0;
+  for (let [o, r, oi, ri] of input) {
+    if (oi != null) {
+      ret += oi * 100;
+    } else if (ri != null) {
+      ret += ri;
+    } else {
+      console.log("???");
+    }
+  } 
+
+  return ret;
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +61,24 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+  {
+    input: `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`,
+    expected: 405,
+  },
     ],
     solution: part1,
   },
