8b8b6eaa36ab1f0725c06efaf211b260e9d0b9d4
Edward Palmer <891364+elp2@users.noreply.github.com>
2023-12-28 15:48:23 +0000

    2023 Day 15 Part 2.

--- 2023/README.md
+++ 2023/README.md
@@ -25,7 +25,7 @@
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
-[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -167,9 +167,9 @@ Both parts: 1124.748ms
 
 ```
 Day 15
-Time part 1: 2.523ms
-Time part 2: -
-Both parts: 2.523ms
+Time part 1: 2.19ms
+Time part 2: 7.407ms
+Both parts: 9.597ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 28/50
-Total time: 1828.644ms
+Total stars: 29/50
+Total time: 1835.718ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day15/index.ts
+++ 2023/src/day15/index.ts
@@ -1,6 +1,26 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  let ret = [];
+  let instructions = rawInput.trim().split(",");
+  for (let ins of instructions) {
+    let name = "";
+    let val = null;
+    for (let i = 0; i < ins.length; i++) {
+      if (ins[i] != "-" && ins[i] != "=") {
+        name += ins[i];
+      } else {
+        if (ins[i] == "=") {
+          val = parseInt(ins.substr(i + 1));
+        }
+        break;
+      }
+    }
+
+    ret.push([name, hashStr(name), val]);
+  }
+  return ret;
+};
 
 const hashStr = (s: string) => {
   const hashChar = (c: string, ival : number) => {
@@ -24,6 +44,11 @@ const hashStr = (s: string) => {
 console.assert(hashStr("H") == 200);
 console.assert(hashStr("HASH") == 52);
 
+let a = [1,2,3];
+console.log(a);
+a.splice(1, 1);
+console.log(a);
+
 const part1 = (rawInput: string) => {
   const input = rawInput.trim().split(",");
   let ret = 0;
@@ -36,10 +61,54 @@ const part1 = (rawInput: string) => {
   return ret;
 };
 
+const focusingPower = (boxes) => {
+  let ret = 0;
+  for (let boxi = 0; boxi < boxes.length; boxi++) {
+    for (let lensi = 0; lensi < boxes[boxi].length; lensi++) {
+      // One plus the box number of the lens in question.
+      let here = 1 + boxi;
+      // The slot number of the lens within the box: 1 for the first lens, 2 for the second lens, and so on.
+      here *= 1 + lensi;
+      // The focal length of the lens.      
+      here *= boxes[boxi][lensi]["focalLength"];
+
+      ret += here;
+    }
+  }
+  return ret;
+};
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  // console.log(input);
 
-  return;
+  let boxes = [];
+  for (let i = 0; i < 256; i++) {
+    boxes.push([]);
+  }
+
+  for (let [name, boxi, focalLength] of input) {
+    let found = false;
+    for (let lensi = 0; lensi < boxes[boxi].length; lensi++) {
+      let here = boxes[boxi][lensi];
+      if (here["name"] == name) {
+        found = true;
+        if (focalLength == null) {
+          boxes[boxi].splice(lensi, 1);
+        } else {
+          boxes[boxi][lensi]["focalLength"] = focalLength;
+        }
+        break;
+      }
+    }
+    if (focalLength != null && !found) {
+      boxes[boxi].push({"name": name, "focalLength": focalLength});
+    }
+
+    // console.log(name, boxi, focalLength, "=> ", boxes[0], boxes[1], boxes[2], boxes[3]);
+  }
+
+  return focusingPower(boxes);
 };
 
 run({
@@ -54,10 +123,10 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`,
+        expected: 145,
+      },
     ],
     solution: part2,
   },
