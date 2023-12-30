d81d0a0c45b4102a5faa0334451fd87b8fe056cb
Edward Palmer <891364+elp2@users.noreply.github.com>
2023-12-30 18:32:07 +0000

    2023 Day 18 Part 2 . Sorted Segments by type.

--- 2023/src/day18/index.ts
+++ 2023/src/day18/index.ts
@@ -5,7 +5,7 @@ const parseInput = (rawInput: string) => {
   let ret = [];
   for (let l of rawInput.split("\n")) {
     let [dir, length, color] = l.split(" ");
-    color = color.substring(1, color.length - 1);
+    color = color.substring(2, color.length - 1);
     ret.push([dir, parseInt(length), color]);
   }
   return ret;
@@ -102,17 +102,87 @@ const part1 = (rawInput: string) => {
       }
     }
   }
-  // for (let [key, value] of Object.entries(holes)) {
-  //   if (!value) {
-  //     console.log("EMPTY: ", key);
-  //   }
-  // }
 
   return ret;
 };
 
+const dirLengthFromHexColor = (color : string) => {
+  console.assert(color.length == 6);
+
+  let length = parseInt(color.substring(0, 5), 16);
+  // 0 means R, 1 means D, 2 means L, and 3 means U.
+  let dir = {"0": "R", "1": "D", "2": "L", "3": "U"}[color[5]];
+  console.log(dir, length);
+  return [dir, length];
+}
+
+const addSegment = (allSegments, horizontal, start, end) => {
+  let cmpI = horizontal ? 0 : 1;
+  let otherI = horizontal ? 1 : 0;
+  console.assert(start[otherI] == end[cmpI]);
+  if (start[cmpI] == end[cmpI]) {
+    console.log("1 length item!!! ", horizontal, start, end);
+  }
+
+  if (start[cmpI] >= end[cmpI]) {
+    [start, end] = [end, start];
+  }
+
+  let index = start[otherI];
+  let segments = allSegments[index];
+  if (segments == null) {
+    segments = [];
+    allSegments[index] = segments;
+  }
+
+  let merged = false;
+  for (let i = 0; i < segments.length; i++) {
+    let [a, b] = segments[i];
+    let aInside = a[cmpI] >= start[cmpI] && a[cmpI] <= end[cmpI];
+    let bInside = b[cmpI] >= start[cmpI] && b[cmpI] <= end[cmpI];
+    if (aInside || bInside) {
+      merged = true;
+      let startZ = Math.min(a[cmpI], start[cmpI]);
+      let endZ = Math.max(b[cmpI], end[cmpI]);
+      if (horizontal) {
+        segments[i] = [[startZ, index], [endZ, index]];
+      } else {
+        segments[i] = [[index, startZ], [index, endZ]];
+      }
+    }
+  }
+  if (!merged) {
+    segments.push([start, end]);
+    segments.sort((a, b) => a[0][cmpI] - b[0][cmpI]);
+  }
+};
+
 const part2 = (rawInput: string) => {
+  let horizontals = {};
+  let verticals = {};
   const input = parseInput(rawInput);
+  let pos = [0, 0];
+  for (let [xDir, xLength, hexColor] of input) {
+    let [dir, length] = dirLengthFromHexColor(hexColor);
+    let [dx, dy] = DIRS[dir];
+    let start = pos;
+    let end = [pos[0] + dx * length, pos[1] + dy * length];
+    if (dy == 0) {
+      addSegment(horizontals, true, start, end);
+    } else {
+      addSegment(verticals, false, start, end);
+    }
+
+    pos = end;
+  }
+
+  for (let [key, value] of Object.entries(horizontals)) {
+    console.log(key, ":", value);
+  }
+  for (let [key, value] of Object.entries(verticals)) {
+    console.log(key, ":", value);
+  }
+  //console.log(horizontals, verticals);
 
   return;
 };
@@ -142,13 +212,26 @@ U 2 (#7a21e3)`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)`,
+  expected: 952408144115,
+},
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 });
