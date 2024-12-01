f80ca473fe70901920e8ad6b0cd315686593ab2d
Edward Palmer <891364+elp2@users.noreply.github.com>
2024-11-30 21:06:07 -0800

    2024 Day 1 Part 1

---
+++ 2024/1.py
@@ -0,0 +1,94 @@
+from collections import defaultdict, deque, Counter
+from itertools import combinations, combinations_with_replacement, permutations
+from functools import reduce, cmp_to_key
+import math
+# import numpy as np
+from operator import add, mul, itemgetter, attrgetter
+import os
+import re
+from sys import argv
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+# Sorts with cmp. cmp < 0 for a < b, == 0 for a==b, > 0 for a > b.
+def elpsort(array, cmp):
+    return sorted(array, key=cmp_to_key(cmp))
+
+ON_TEXT = '\u2588'
+OFF_TEXT = '\u2592'
+
+LEFT = 0
+UP = 1
+RIGHT = 2
+DOWN = 3
+CHAR_TO_DS = {"^": UP, ">": RIGHT, "<": LEFT, "v": DOWN}
+DS = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+DS8 = DS + [(-1, -1), (1, -1), (-1, 1), (1, 1)]
+
+def arounds(x, y, diagonals):
+    ret = []
+    for dx, dy in DS8 if diagonals else DS:
+        ret.append((x + dx, y + dy))
+    return ret
+
+CHALLENGE_DAY = argv[0].split("/")[-1].replace(".py", "").replace("p2.py", "")
+AOC_DIR = os.path.dirname(argv[0])
+
+print("Day: ", CHALLENGE_DAY)
+
+######################
+SAMPLE_EXPECTED = 11
+######################
+assert SAMPLE_EXPECTED != None
+
+try:
+    SAMPLE = open(os.path.join(AOC_DIR, CHALLENGE_DAY + ".s.txt")).read()
+except:
+    print("Did you create the sample file?")
+
+try:
+    REAL = open(os.path.join(AOC_DIR, CHALLENGE_DAY + ".txt")).read()
+except:
+    print("Did you create the solutions file?")
+
+
+def parse_lines(raw):
+    # Groups.
+    # groups = raw.split("\n\n")
+    # return list(map(lambda group: group.split("\n"), groups))
+    lines = raw.split("\n")
+    # return lines # raw
+    z = list(map(lambda l: l.split(" "), lines)) # words.
+    ret = []
+    aes, bes = [], []
+    print(z)
+
+    for line in z:
+        l = [x for x in line if x != ""]
+        a, b = l
+        aes.append(int(a))
+        bes.append(int(b))
+    return aes, bes
+
+
+def solve(raw):
+    aes, bes = parse_lines(raw)
+    aes = sorted(aes)
+    bes = sorted(bes)
+    
+    ret = 0
+    for a, b in zip(aes, bes):
+        diff = abs(a - b)
+        ret += diff
+        print(a,b, diff)
+    return ret
+
+sample = solve(SAMPLE)
+if sample != SAMPLE_EXPECTED:
+    print("SAMPLE FAILED: ", sample, " != ", SAMPLE_EXPECTED)
+assert sample == SAMPLE_EXPECTED
+print("\n*** SAMPLE PASSED ***\n")
+
+solved = solve(REAL)
+print("SOLUTION: ", solved)
