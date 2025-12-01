29fd58ecc6e5bafb56001e8c4787c646e1ab430a
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-11-30 21:24:05 -0800

    2025 Day 1.

---
+++ 2025/1p2.py
@@ -0,0 +1,66 @@
+from collections import defaultdict, deque, Counter
+from itertools import combinations, combinations_with_replacement, permutations, product
+from functools import reduce, cmp_to_key
+from operator import add, mul, itemgetter, attrgetter
+import os
+from parse import parse
+import sys
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, root_dir)
+from aoc_elp import *
+
+######################
+SAMPLE_EXPECTED = 6
+######################
+assert SAMPLE_EXPECTED != None, "Must enter sample value"
+
+def parse_group(group):
+    lines = group.split("\n")
+    ret = []
+    for y, line in enumerate(lines):
+        line = line.strip()
+        assert len(line) != 0
+        split = (line[0], int(line[1:]))
+
+        assert len(split) != 0
+        ret.append(split)
+
+    return ret
+
+
+def parse_lines(raw):
+    # Groups.
+    # groups = raw.split("\n\n")
+    # Do something with the groups.
+
+    return parse_group(raw)
+
+
+def solve(raw):
+    parsed = parse_lines(raw)
+    print(parsed)
+
+    ret = 0
+    at = 50
+    for dir, num in parsed:
+        print(dir, num, at)
+        for _ in range(num):
+            delta = 1 if dir == "R" else -1
+            at = (at + delta + 100) % 100
+            if at == 0 or at == 100:
+                ret += 1
+                at = 0
+        print(at)
+
+    return ret
+
+if __name__ == "__main__":
+    SAMPLE, REAL = get_raw_inputs(sys.argv)
+
+    sample = solve(SAMPLE)
+    assert sample == SAMPLE_EXPECTED, "Sample Result %s != %s expected" % (sample, SAMPLE_EXPECTED)
+    print("\n*** SAMPLE PASSED ***\n")
+
+    solved = solve(REAL)
+    print("SOLUTION: ", solved)
---
+++ 2025/README.md
@@ -0,0 +1,7 @@
+# Advent of Code Solutions 2025
+
+## Summary
+Just 12 days this time, better than 0!
+
+## Day 1
+Definitely harder than the normal Day 1. The second part was small enough that you could easily brute force it if you were still warming up on problems :) 
