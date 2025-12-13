c6666e3ca5c44b1e5e1112a9cbf8eb965c8d9ea2
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-12-13 10:51:03 -0800

    2025 Day 10 Part 1.

---
+++ 2025/10.py
@@ -0,0 +1,76 @@
+from collections import defaultdict, deque, Counter
+from itertools import combinations, combinations_with_replacement, permutations, product
+from functools import reduce, cmp_to_key
+from operator import add, mul, itemgetter, attrgetter
+import os
+from parse import parse
+import sys
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, root_dir)
+from aoc_elp import *
+
+######################
+SAMPLE_EXPECTED = 7
+######################
+assert SAMPLE_EXPECTED != None, "Must enter sample value"
+
+def parse_group(group):
+    lines = group.split("\n")
+    ret = []
+    for y, line in enumerate(lines):
+        line = line.strip()
+        assert len(line) != 0
+
+        split = line.split(" ")
+        lights = [b == "#" for b in split[0][1:-1]]
+        buttons = split[1:-1]
+        buttons = [b.replace("(", "[").replace(")", "]") for b in buttons]
+        buttons = [set(eval(b)) for b in buttons]
+        joltage = list(map(int, split[-1][1:-1].split(",")))
+
+        assert len(split) != 0
+        ret.append((lights, buttons, joltage))
+
+    return ret
+
+
+def parse_lines(raw):
+    # Groups.
+    # groups = raw.split("\n\n")
+    # Do something with the groups.
+
+    return parse_group(raw)
+
+def solve(raw):
+    parsed = parse_lines(raw)
+    print(parsed)
+
+    ret = 0
+    for lights, buttons, joltage in parsed:
+        here = 0
+        for cl in range(len(lights)):
+            for button_combo in combinations(buttons, cl):
+                lnew = [False] * len(lights)
+                for button in button_combo:
+                    for bi in button:
+                        lnew[bi] = not lnew[bi]
+                if lnew == lights:
+                    here = len(button_combo)
+                    break
+            if here != 0:
+                break
+        assert here != 0
+        ret += here
+
+    return ret
+
+if __name__ == "__main__":
+    SAMPLE, REAL = get_raw_inputs(sys.argv)
+
+    sample = solve(SAMPLE)
+    assert sample == SAMPLE_EXPECTED, "Sample Result %s != %s expected" % (sample, SAMPLE_EXPECTED)
+    print("\n*** SAMPLE PASSED ***\n")
+
+    solved = solve(REAL)
+    print("SOLUTION: ", solved)
