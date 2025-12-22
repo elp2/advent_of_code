0e4352a11bf7cb882c841a27ad3c6c6af0d04252
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-12-22 13:59:21 -1000

    2025 Day 12 optimizations.

--- 2025/12.py
+++ 2025/12.py
@@ -1,9 +1,10 @@
 from collections import defaultdict, deque, Counter
 from itertools import combinations, combinations_with_replacement, permutations, product
-from functools import reduce, cmp_to_key
+from functools import reduce, cmp_to_key, lru_cache
 from operator import add, mul, itemgetter, attrgetter
 import os
 import sys
+import cProfile
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, root_dir)
@@ -38,7 +39,7 @@ def shape_perms(shape):
         r = [[0] * len(s) for _ in range(len(s))]
         for y in range(len(s)):
             for x in range(len(s)):
-                r[x][y] = s[y][x]
+                r[x][len(s)-1-y] = s[y][x]
         return r
         
     def flip(s):
@@ -49,10 +50,12 @@ def shape_perms(shape):
         return r
 
     def addnew(r, to):
-        equals = [str(o) == str(r) for o in to]
-        if any(equals):
+        if r in to:
             return
         to.append(r)
+
+    def binify(sha):
+        return [int("".join(sh), 2) for sh in sha]
 
     for _ in range(4):
         flipped = flip(shape)
@@ -60,12 +63,12 @@ def shape_perms(shape):
         addnew(flipped, ret)
         addnew(shape, ret)
     
-    # Ensure first item has a top left #
-    if ret[0][0][0] != True:
-        for i in range(len(ret)):
-            if ret[i][0][0]:
-                ret[i], ret[0] = ret[0], ret[i]
-                return ret
+    # # Ensure first item has a top left #
+    # if ret[0][0][0] != True:
+    #     for i in range(len(ret)):
+    #         if ret[i][0][0]:
+    #             ret[i], ret[0] = ret[0], ret[i]
+    #             return ret
 
     return ret
 
@@ -78,7 +81,7 @@ def parse_lines(raw):
         shapelines = sraw.split("\n")[1:]
         shape = []
         for sl in shapelines:
-            shape.append(list("1" if c == "#" else "0" for c in sl))
+            shape.append([c == "#" for c in sl])
         
         shapes.append(shape_perms(shape))
         
@@ -96,18 +99,27 @@ def parse_lines(raw):
 
 
 def slide_across(w, h, shapes):
+    shapedim = len(shapes[0][0])
     ret = []
     for shape in shapes:
+        # print("---------", shape)
         ints = []
         for shapeperm in shape:
-            for gy in range(h - len(shapeperm) + 1):
-                for gx in range(w - len(shapeperm) + 1):
-                    grid = [["0"] * w for _ in range(h)]
-                    for y in range(len(shapeperm)):
-                        for x in range(len(shapeperm)):
-                            grid[gy + y][gx + x] = shapeperm[y][x]
-                    gbin = "".join(["".join(g) for g in grid])
-                    ints.append((int(gbin, 2), gx, gy))
+            for gx in range(w - len(shapeperm) + 1):
+                for gy in range(h - len(shapeperm) + 1):
+                    grid = 0
+                    for y in range(shapedim):
+                        for x in range(shapedim):
+                            if shapeperm[y][x]:
+                                bit = 1 << (gx + x + (gy + y) * w)
+                                grid |= bit
+                    if False:
+                        gbin = bin(grid)[2:]
+                        gbin = "0" * ((w * h) - len(gbin)) + gbin
+                        for py in range(h):
+                            print(gbin[py * w:(py + 1) * w])
+                        print("-------")
+                    ints.append((grid, gx, gy))
         ret.append(ints)
     return ret
 
@@ -117,38 +129,49 @@ def solve1(shapes, grid):
     ret = 0
     slides = slide_across(w, h, shapes)
 
-    shapecounts = [str(s[0]).count("1") for s in shapes]
-    bad = int("1" + "0" * (w - 1) + "1" + "0" * (w - 1) + "1" + "0" * (w - 1), 2)
+    shapecounts = [str(s[0]).count("T") for s in shapes]
     shapesize = len(shapes[0][0])
 
-    def dfs(grid, counts, used, pgx, pgy):
+    @lru_cache(maxsize=None)
+    def dfs(grid, counts):
         if sum(counts) == 0:
             return 1
         needs = [c * counts[i] for i, c in enumerate(shapecounts)]
         needed = sum(needs)
-        # hremain = h - pgy - 1
-        if w * h < used + needed: # or hremain * w < needed:
-            # print(hremain * w)
-            return 0
-        if used and not bad & grid:
+        used = grid.bit_count()
+        if w * h < used + needed:
             return 0
 
-        new_counts = list(counts)
+        best_fits = None
+        best_i = None
+
         for i, c in enumerate(counts):
             if c == 0:
                 continue
-            for (cand, gx, gy) in slides[i]:
-                if gy == pgy and gx - pgx > 2:
-                    continue
+            fits = []
+            for (cand, _, _) in slides[i]:
                 if cand & grid == 0:
-                    new_counts[i] -= 1
-                    if dfs(cand | grid, new_counts, used + shapecounts[i], gx, gy):
-                        return 1
-                    new_counts[i] += 1
+                    fits.append(cand)
+                    if best_fits is not None and len(best_fits) < len(fits):
+                        break
+            
+            if best_fits is None or len(fits) < len(best_fits):
+                best_fits = fits
+                best_i = i
+
+        if best_fits is None:
+            return 0
+        
+        new_counts = list(counts)
+        new_counts[best_i] -= 1
+        new_counts = tuple(new_counts)
+        for cand in best_fits:
+            if dfs(cand | grid, new_counts):
+                return 1
         
         return 0    
 
-    return dfs(0, base_counts, 0, -100, -100)
+    return dfs(0, tuple(base_counts))
 
 
 def solve(raw):
